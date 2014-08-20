fc2688d0f5a5858ece8167ca9d5dc99fb7c74233
AnthonyCorrado <amcstudios@yahoo.com>
2014-08-20 01:17:57 -0700

    small nav bar fx added

--- app/assets/javascripts/custom.js
+++ app/assets/javascripts/custom.js
@@ -1,14 +1,16 @@
 $('.about-me').click(function(){
-    $('.main-content-sub').slideToggle('slow');
+  $('.main-content-sub').slideToggle('slow');
 });
 $('.about-me-text').click(function(){
-    $('.main-content-sub').slideToggle('slow');
+  $('.main-content-sub').slideToggle('slow');
 });
 $('.portfolio').click(function(){
-    $('.projects-content-sub').slideToggle(1000);
+	$('.projects-content').animate({'width': '90%'});
+  $('.projects-content-sub').delay(400).slideToggle(1000);
 });
 $('.portfolio-text').click(function(){
-    $('.projects-content-sub').slideToggle(1000);
+	$('.projects-content').animate({'width': '90%'});
+  $('.projects-content-sub').delay(400).slideToggle(1000);
 });
 $('.portfolio').hover(
 	function(){
@@ -16,7 +18,7 @@ $('.portfolio').hover(
 	},
 	function(){
 		$('.portfolio-text').css({
-			'color': '#FEFEFE'
+			'color': '#DDDDDD'
 		}, 'slow');
   }
 );
@@ -26,7 +28,7 @@ $('.portfolio-text').hover(
 	},
 	function(){
 		$('.portfolio-text').css({
-			'color': '#FEFEFE'
+			'color': '#DDDDDD'
 		}, 'slow');
   }
 );
@@ -36,7 +38,7 @@ $('.portfolio-text').hover(
 	},
 	function(){
 		$('.portfolio-text').css({
-			'color': '#FEFEFE'
+			'color': '#DDDDDD'
 		}, 'slow');
   }
 );
@@ -46,7 +48,7 @@ $('.about-me').hover(
 	},
 	function(){
 		$('.about-me-text').css({
-			'color': '#FEFEFE'
+			'color': '#DDDDDD'
 		}, 'slow');
   }
 );
@@ -56,7 +58,37 @@ $('.about-me-text').hover(
 	},
 	function(){
 		$('.about-me-text').css({
-			'color': '#FEFEFE'
+			'color': '#DDDDDD'
 		}, 'slow');
+  }
+);
+$('.red5').hover(
+	function(){
+		$('.red5').css({'color': 'steelblue'}, 1000);
+	},
+	function(){
+		$('.red5').css({
+			'color': '#DDDDDD'
+		}, '2000');
+  }
+);
+$('.red9').hover(
+	function(){
+		$('.red9').css({'color': 'steelblue'}, 1000);
+	},
+	function(){
+		$('.red9').css({
+			'color': '#DDDDDD'
+		}, '2000');
+  }
+);
+$('.red14').hover(
+	function(){
+		$('.red14').css({'color': 'steelblue'}, 1000);
+	},
+	function(){
+		$('.red14').css({
+			'color': '#DDDDDD'
+		}, '2000');
   }
 );
