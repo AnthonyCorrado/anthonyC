6fc3ff7e84589b256baf650506a6b1d42cd6f21d
AnthonyCorrado <amcstudios@yahoo.com>
2014-08-28 16:42:32 -0700

    added save to home

--- app/controllers/home_pages_controller.rb
+++ app/controllers/home_pages_controller.rb
@@ -4,4 +4,19 @@ class HomePagesController < ApplicationController
 		@user = User.new
 	end
 
+	def create
+		@user = User.new(user_params)
+
+		respond_to do |format|
+			if @user.save
+				UserMailer.welcome_email(@user).deliver
+				format.html { redirect_to(root_path, notice: 'Email was successful') }
+        format.json { render json: @user, status: :created, location: @user }
+      else
+        format.html { redirect_to root_path }
+        format.json { render json: @user.errors, status: :unprocessable_entity }
+      end
+    end
+  end
+
 end
