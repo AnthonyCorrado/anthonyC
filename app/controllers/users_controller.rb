dabd984d0f8fd4f62607936e2aea016072778b65
AnthonyCorrado <amcstudios@yahoo.com>
2014-08-28 15:05:54 -0700

    user model hack added

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -10,12 +10,17 @@ class UsersController < ApplicationController
 
 	def create
 		@user = User.new(user_params)
+
+		respond_to do |format|
 			if @user.save
 				UserMailer.welcome_email(@user).deliver
-        redirect_to url_for(:controller => :home_pages, :action => :index)
+				format.html { redirect_to(root_path, notice: 'Email was successful') }
+        format.json { render json: @user, status: :created, location: @user }
       else
-        redirect_to url_for(:controller => :home_pages, :action => :index)
+        format.html { render action: 'new' }
+        format.json { render json: @user.errors, status: :unprocessable_entity }
       end
+    end
   end
 
   def show
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,7 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+User.last.destroy
+User.create(name: "anthony", email: "any@email.com", message: "default")
+
